2184e974ff00497ba2f8a44b4af00833fc72a6d8
petref <petroupb@gmail.com>
2024-07-04 18:30:28 +0300

    update entry point and app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import graphqlServer from './graphql/index.js';
 
 const app = express();
+
+graphqlServer(app);
 
 export default app;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 import app from './app.js';
 import { port } from "./config/environment/index.js";
+import sequelize from './db/index.js';
+
+
+
 const start = async () => {
   try {
+    sequelize.authenticate().then(() => {
+        console.log('Connection has been established successfully.');
+     }).catch((error) => {
+        console.error('Unable to connect to the database: ', error);
+     });
     app.listen(port);
     console.log(`server running at port: ${port}`);
   } catch {
