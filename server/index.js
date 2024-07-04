f6d3f327b460268e990e810cddbc42660eb00840
petref <petroupb@gmail.com>
2024-07-04 13:29:48 +0300

    Add point of entry for server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-import 'dotenv/config'
+import app from './app.js';
+import { port } from "./config/environment/index.js";
+const start = async () => {
+  try {
+    app.listen(port);
+    console.log(`server running at port: ${port}`);
+  } catch {
+    console.log('Not able to run server');
+  }
+};
+
+start();
